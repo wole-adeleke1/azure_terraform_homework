84adc360a70055eedc8bbc35da1fce5a1ee1f61d
James Makepeace <James.Makepeace@relexsolutions.com>
2021-11-24 10:45:04 +0000

    corrected name

--- Terraform/Subscriptions/Subscription_A/main.tf
+++ Terraform/Subscriptions/Subscription_A/main.tf
@@ -28,7 +28,7 @@ resource "azurerm_subnet" "example" {
 }
 
 resource "azurerm_network_security_group" "example" {
-  name                = "Sub_1_NSG"
+  name                = "Sub_A_NSG"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
